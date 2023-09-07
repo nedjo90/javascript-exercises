40053ca3571223a6766f4698f57c52f956449698
Necati Han <han.necati@gmail.com>
2023-09-07 17:16:10 +0200

    accomplish get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+/* const books = [
+	{
+		title: 'Book',
+		author: 'Name'
+	},
+	{
+		title: 'Book2',
+		author: 'Name2'
+	}
+] */
 
-};
+const getTheTitles = function (obj) {
+	const myArray = [];
+	for (let i = 0; i < obj.length; i++) {
+		myArray[i] = obj[i].title;
+		//console.log(obj[i].title);
+	}
+	//console.log(myArray);
+	return myArray;
+}
 
+//getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
