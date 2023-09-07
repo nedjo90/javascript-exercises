ec055384177bb54201b1c40eb0d09fc1fa89e9ae
Necati Han <han.necati@gmail.com>
2023-09-07 16:23:11 +0200

    accomplish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function palindromes(str) {
+	let cleanStr = str.replace(/[^a-z0-9]/gi, "");
+	cleanStr = cleanStr.toLowerCase();
+	let reverted = "";
+	let i = cleanStr.length - 1;
+	while (i >= 0) {
+		reverted += cleanStr[i--];
+	}
+	return (reverted === cleanStr ? true : false);
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
