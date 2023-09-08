5d7a69a31793f7425b2435904cefe15ec919a039
Necati Han <han.necati@gmail.com>
2023-09-08 09:34:34 +0200

    accomplish find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
-
+const people = [
+	{
+		name: "Carly",
+		yearOfBirth: 1942,
+		yearOfDeath: 1970,
+	},
+	{
+		name: "Ray",
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+	{
+		name: "Jane",
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+]
+//console.table(people);
+const findTheOldest = function (obj) {
+	const ordered = obj.sort((a, b) => {
+		let aAge;
+		let bAge;
+		if (!a.yearOfDeath) {
+			aAge = ((new Date()).getFullYear()) - a.yearOfBirth;
+		}
+		else {
+			aAge = a.yearOfDeath - a.yearOfBirth;
+		}
+		if (!b.yearOfDeath) {
+			bAge = ((new Date()).getFullYear()) - b.yearOfBirth;
+		}
+		else {
+			bAge = b.yearOfDeath - b.yearOfBirth;
+		}
+		return (aAge > bAge ? -1 : 1);
+	});
+	return obj[0];
 };
+//console.log(findTheOldest(people));
+//console.table(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
